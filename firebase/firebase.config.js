7088cbef700a8b7c0dc5270a91ed4aa2dbdda3b7
Kinakopan <mtakagi@my.bcit.ca>
2022-11-02 14:50:28 -0700

    hid all the others

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyChoXqfva42ziSxyQTRgxDKC2V1-n8Q8-g",
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: "login-with-firebase-8dd28.firebaseapp.com",
-  projectId: "login-with-firebase-8dd28",
-  storageBucket: "login-with-firebase-8dd28.appspot.com",
-  messagingSenderId: "234966019141",
-  appId: "1:234966019141:web:b5d4d804b90397e259472c"
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId
 };
 
 const app = initializeApp(firebaseConfig);
